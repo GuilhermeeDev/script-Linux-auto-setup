e1c46e0a714f52d2a0ac46aff99b1af351fefba8
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-06 19:06:42 -0400

    update (main) main da aplicação, rodando os processos de configuração tudo certinho.

--- src/main.sh
+++ src/main.sh
@@ -1,7 +1,13 @@
-#! /bin/bash
-echo "<------------------------------------------------------------------------------>"
-echo "| Ola, bem vindo ao meu script de configuração para distros baseadas em Linux! |"
-echo "<------------------------------------------------------------------------------>"
+#!/bin/bash
+
+for f in ./config/*.sh; do
+    chmod +x "$f"
+done
+
+echo "================================================================================"
+echo "= Ola, bem vindo ao meu script de configuração para distros baseadas em Linux! ="
+echo "================================================================================"
+
 echo "Para contribuit com esse projeto acesse: https://github.com/GuilhermeeDev/script-configuration-for-linux.git"
 echo ""
 
