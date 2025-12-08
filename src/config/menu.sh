87ef8ef946585fd5b7718aeced839d65838cec49
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-08 16:06:28 -0400

    update(menu options)

--- src/config/menu.sh
+++ src/config/menu.sh
@@ -1,32 +1,23 @@
 #!/bin/bash
 # shellcheck disable=SC1091
 source ./config/functions.sh
+clear
 
 echo "=========================== MENU ==========================="
-
-echo -e "1. Instalar pacotes basicos para uso do linux.\n2. Instalar pacotes de desenvolvedor para linux.\n3. Instalar 'beautifulLinux'(beta)\n4. Pacotes [.appImage;.deb;]\n5. Sair. "; read op
-export op
+echo -e "1. Instalar pacotes basicos para uso do linux.\n\
+2. Instalar pacotes de desenvolvedor para linux.\n\
+3. Instalar 'beautifulLinux'(beta)\n\
+4. Instalar pacotes |.appImage | .deb |\n\
+5. Sair. "; read op
 
 case "${op}" in
     1)
-        echo "=========================== BasicPackages ==========================="
-        install_basicPackages
+        echo "=========================== Install BasicPackages ==========================="
+        install_basic_packages
     ;;
     2)
-        echo "=========================== DevPackages ==========================="
-        install_devPackages
-    ;;
-    3)
-        echo "item 3"
-    ;;
-    4)
-        install_packages_lfs
-
-    ;;
-    5)
-        echo "Saindo.."
-        exit
-
+        echo "=========================== Install DevPackages ==========================="
+        install_dev_packages
     ;;
     *)
         echo "Saindo.."
