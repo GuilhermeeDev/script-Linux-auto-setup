9c30ce7e54ccc4f70dd2ee4b1d27300539b92cdb
GuilhermeeDev <guilhermee.dev.software@gmail.com>
2025-12-09 10:33:10 -0400

    update (op no menu) Update no meu e nos diretorios reconhecidos pelo .gitgnore.

--- src/config/menu.sh
+++ src/config/menu.sh
@@ -6,20 +6,25 @@ clear
 echo "=========================== MENU ==========================="
 echo -e "1. Instalar pacotes basicos para uso do linux.\n\
 2. Instalar pacotes de desenvolvedor para linux.\n\
-3. Instalar 'beautifulLinux'(beta)\n\
-4. Instalar pacotes |.appImage | .deb |\n\
+3. Adicionar repositorios com base na distro.\n\
+4. Instalar 'beautifulLinux'(beta)\n\
 5. Sair. "; read op
 
 case "${op}" in
     1)
         echo "=========================== Install BasicPackages ==========================="
-        install_basic_packages
+        basic_packages
     ;;
     2)
         echo "=========================== Install DevPackages ==========================="
-        install_dev_packages
+        dev_packages
+    ;;
+    3)
+        echo "=========================== Install Repositories ==========================="
+        ./config/add_repos.sh
     ;;
     *)
-        exit
+        clear
+        exit 1
     ;;
 esac
